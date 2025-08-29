4d63114e224c5e89ffba8aab0e6e48c6bbb333b6
rugved0102 <rugvedjunghare@gmail.com>
2025-08-29 22:31:44 +0530

    basics

--- 01-basic-backend-app/index.js
+++ 01-basic-backend-app/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-const axios = require('axios')
+require('dotenv').config() // loads environment variables from a .env file into process.env.
+const axios = require('axios') // for http requests
 const express = require('express')
-const app = express()
+const app = express() // app is now your server that can define routes and listen for requests.
 const port = 3000
 
 app.get('/',(req, res) => {
@@ -16,6 +16,8 @@ app.get('/activity', async (req, res) => {
     try {
         const response = await axios.get('https://bored-api.appbrewery.com/random')
         res.json(response.data)
+        // console.log(response)
+        // console.log(response.data)
     } catch (error) {
         res.status(500).send('Error fetching data')
     }
