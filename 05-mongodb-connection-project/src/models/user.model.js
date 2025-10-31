d609fb5fbfce5d9de20ee103c788f81d850b1e28
rugved0102 <rugvedjunghare@gmail.com>
2025-10-31 22:05:34 +0530

    user model done

--- 05-mongodb-connection-project/src/models/user.model.js
+++ 05-mongodb-connection-project/src/models/user.model.js
@@ -1,2 +1,52 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, // cludinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String,
+    }
+}, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+    this.password = bcrypt.hash(this.password)
+})
+
+export const User = mongoose.model("User", userSchema)
