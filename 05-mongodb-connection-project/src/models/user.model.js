84497940aeb67eef270590b4f26fd82eb503f826
rugved0102 <rugvedjunghare@gmail.com>
2025-12-10 19:19:52 +0530

    jwt tokens added

--- 05-mongodb-connection-project/src/models/user.model.js
+++ 05-mongodb-connection-project/src/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true,
     },
-    email: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -54,6 +54,36 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
