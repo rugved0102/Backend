bb8bc7076a8d23a27bac1c53b5f65174d7e3d67e
rugved0102 <rugvedjunghare@gmail.com>
2025-12-10 19:00:25 +0530

    user schema password encryption done using bcrypt

--- 05-mongodb-connection-project/src/models/user.model.js
+++ 05-mongodb-connection-project/src/models/user.model.js
@@ -46,7 +46,14 @@ const userSchema = new Schema({
 }, { timestamps: true })
 
 userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
     this.password = bcrypt.hash(this.password)
+    next()
 })
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema)
